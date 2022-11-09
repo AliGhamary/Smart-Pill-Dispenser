d9c0642bc05cd1011db64402d220b97db3973718
hjauregui7 <jaureguihector66@gmail.com>
2022-11-09 10:11:51 -0600

    Test commit

--- new_Ui.py
+++ new_Ui.py
@@ -625,6 +625,7 @@ class controller():
       
       GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
       GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+#This is a test
 
 if __name__ == '__main__':
     a = HomeScreen(0)
